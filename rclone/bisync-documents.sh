01b57dcee2ef31d4a36a3428f9f42246a5ff3f14
Wei Song <songweicn@gmail.com>
2026-07-14 13:08:52 -0500

    configure SOPS

--- rclone/bisync-documents.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-exec /run/current-system/sw/bin/rclone bisync \
-    onedrive:Documents \
-    "$HOME/Documents" \
-    --create-empty-src-dirs \
-    "$@"
--- rclone/mount-pictures.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-exec /run/current-system/sw/bin/rclone mount \
-    onedrive:Pictures \
-    "$HOME/Pictures" \
-    --vfs-cache-mode full
